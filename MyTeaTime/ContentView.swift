d390e873092f6f53535b03c7e945b4cdc52898d8
Lyosha's MacBook <lyoshasmacbook@10-141-41-75.0101.pos.kr.iosda.org>
2024-04-16 17:46:53 +0900

    Save: 중간작업 저장

--- MyTeaTime/ContentView.swift
+++ MyTeaTime/ContentView.swift
@@ -26,27 +26,40 @@ struct Memo {
 
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+//    @Environment(\.modelContext) private var modelContext
+//    @Query private var items: [Item]
+//    @Query private var TeaTimeBooks: [TeaTimeBook]
+    @State var TeaTimeBooks: [TeaTimeBook] = [
+        TeaTimeBook(title : "나의 커피북",
+                    createdTime : Date.now,
+                    MemoArray : [])
+    ]
+    
+    
     var body: some View {
-        NavigationSplitView {
+        //select TeaTimeBook first
+        var selectedTeaTimeBook = TeaTimeBooks.last
+        NavigationStack {
+            // 여기에 선택된 id의 TeaTimeBook의 아이템을 뿌려주기
+            // TeaTimeBook의 element는 Memo임 !
+            
             List {
-                ForEach(items) { item in
+                ForEach (selectedTeaTimeBook) { item in
                     NavigationLink {
                         Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
                     } label: {
                         Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
                     }
                 }
-                .onDelete(perform: deleteItems)
+//                .onDelete(perform: deleteItems)
+                
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
                 ToolbarItem {
-                    Button(action: addItem) {
+                    Button(action: {print("add")}) {
                         Label("Add Item", systemImage: "plus")
                     }
                 }
@@ -56,20 +69,20 @@ struct ContentView: View {
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+//    private func addItem() {
+//        withAnimation {
+//            let newItem = Item(timestamp: Date())
+//            modelContext.insert(newItem)
+//        }
+//    }
+//
+//    private func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+//            for index in offsets {
+//                modelContext.delete(items[index])
+//            }
+//        }
+//    }
 }
 
 #Preview {
